bcddfd53c5051a316a1c8c431b987da9556d5986
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-06-28 15:54:52 +0200

    printed list of movie in homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                {{ $movie }}
+            </li>    
+        @endforeach
+    </ul>
+
+@endsection
